26f597a3444252474580d179cdafb4d15b3a0baa
rakshaa-t <rakshatated98@gmail.com>
2025-10-18 23:16:05 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import PortfolioHeroSection from "../components/PortfolioHeroSection";
+import "../styles/globals.css";
+
+export default function Page() {
+  return (
+    <main>
+      <PortfolioHeroSection />
+    </main>
+  );
+}
